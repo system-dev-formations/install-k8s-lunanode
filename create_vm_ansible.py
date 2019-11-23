d531a9e1df647b313a6570bf14ed0180a9e02aad
plb-formation <dockerlite@gmail.com>
2019-11-23 14:37:51 +0100

    minor changes

--- create_vm_ansible.py
+++ create_vm_ansible.py
@@ -23,16 +23,16 @@ def create_centos_controller(name):
     api.request("vm", "create",
                 {'hostname': name, 'plan_id': 4, 'region': 'roubaix', 'image_id': 148508, 'storage': 70})
 
-project_name = 'ansible-oxiane-'
+project_name = 'ansible-ambient-'
 user_number = input("Numero du cluster ansible ? ")
 user_number= str(user_number)
-create_centos_controller(project_name + "controller-" +  user_number)
-create_centos_remote(project_name + "remote-centos-" +  user_number)
+#create_centos_:controller(project_name + "controller-" +  user_number)
+create_centos_remote(project_name + "remote-" +  user_number)
 #create_ubuntu_remote(project_name + "remote-ubuntu-" +  user_number)
-
+time.sleep(240)
 results = api.request('vm', 'list')
-f = open(r"/home/hme/inventory_lunanode_oxiane_" + user_number, "w+")
-hfile = open(r"/home/hme/user_list_k8s_oxiane_" + user_number, "w+")
+f = open(r"/home/hme/inventory_lunanode_ambient_" + user_number, "w+")
+hfile = open(r"/home/hme/user_list_k8s_ambient_" + user_number, "w+")
 val = results.get("vms")
 user_dic = {}
 print
@@ -42,7 +42,7 @@ for i in range(0, len(val)):
     for key, value in val[i].items():
         if key == 'name':
             #search= "-" + user_number
-            if user_number not in value:
+            if "remote" not in value:
                 break
             print('name=', value)
             user = value
